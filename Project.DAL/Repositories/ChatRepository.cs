e736bd2411b931b01cb2058b36e91473b73e9f3a
Yucyj <yousraj2016@gmail.com>
2026-08-27 13:52:32 +0300

    add reposotries and interfaces

---
+++ Project.DAL/Repositories/ChatRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.DAL.Data;
+using Project.DAL.Entities;
+using Project.DAL.Interfaces;
+
+namespace Project.DAL.Repositories;
+
+public class ChatRepository : IChatRepository
+{
+    private readonly AppDbContext _context;
+
+    public ChatRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ChatMessage> AddMessageAsync(ChatMessage message)
+    {
+        _context.ChatMessages.Add(message);
+        await _context.SaveChangesAsync();
+        return message;
+    }
+
+    public async Task<List<ChatMessage>> GetHistoryAsync()
+    {
+        return await _context.ChatMessages
+            .Include(m => m.Sender)
+            .OrderBy(m => m.Timestamp)
+            .ToListAsync();
+    }
+
+    public async Task<ApplicationUser?> GetUserByIdAsync(string userId)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+    }
+}
